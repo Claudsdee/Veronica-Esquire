8eefb20720a6489a55cb49ee558deb6248a97d4e
Claudsdee <38685313+Claudsdee@users.noreply.github.com>
2018-04-28 20:05:42 +1000

    edits to css

---
+++ scripts/scroll.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+//     $('#container').scrollify.next();
+// });
+
+new onePageScroll({options})
+document.addEventListener('DOMContentLoaded', function() {
+    var app = new onePageScroll({
+      el: document.querySelectorAll('section'),
+    })
+  })
