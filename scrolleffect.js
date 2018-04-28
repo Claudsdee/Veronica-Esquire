9922752f21ecd9ea6a6fd844744e663c8f050f80
Claudsdee <38685313+Claudsdee@users.noreply.github.com>
2018-04-28 20:37:03 +1000

    scroll

---
+++ scrolleffect.js
@@ -0,0 +1,37 @@
+$.scrollify({
+    section : ".parts",
+    sectionName : "logo",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset : 0,
+    scrollbars: false,
+    standardScrollElements: "",
+    setHeights: false,
+    overflowScroll: false,
+    updateHash: true,
+    touchScroll:true,
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
+});
+
+// $.scrollify.next({
+//     section : ".parts",
+//     sectionName : "about",
+//     interstitialSection : "",
+//     easing: "easeOutExpo",
+//     scrollSpeed: 1100,
+//     offset : 0,
+//     scrollbars: true,
+//     standardScrollElements: "",
+//     setHeights: true,
+//     overflowScroll: true,
+//     updateHash: true,
+//     touchScroll:true,
+//     before:function() {},
+//     after:function() {},
+//     afterResize:function() {},
+//     afterRender:function() {}
+// });
